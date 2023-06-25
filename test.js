498866349d26bc415117f6f0b1317b213619885c
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-06-25 14:58:51 +0700

    1

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var responseJson = JSON.parse($response.body);
-var totalUsd = responseJson.data.totalUsd;
+var totalUsd = responseJson.totalUsd;
 console.log(totalUsd);
 $notification.post('通知', '余额', totalUsd);
 $done({});
