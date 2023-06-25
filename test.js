0b3e97fd85c362a374e79d6dbfd94a451ed23f9d
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-06-25 15:11:27 +0700

    1

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 import ('https://raw.githubusercontent.com/tesaq1/surge/main/test1.js')
-var responseJson = JSON.parse($response.body);
+// var responseJson = JSON.parse($response.body);
 
-console.log(responseJson); // 输出整个响应体以检查其结构
+// console.log(responseJson); // 输出整个响应体以检查其结构
 
-if (responseJson && responseJson.data && responseJson.data.hasOwnProperty('totalUsd')) {
-    var totalUsd = responseJson.data.totalUsd;
-    console.log(totalUsd); // 输出 totalUsd 的值
-} else {
-    console.log('data or totalUsd property not found in the response');
-}
+// if (responseJson && responseJson.data && responseJson.data.hasOwnProperty('totalUsd')) {
+//     var totalUsd = responseJson.data.totalUsd;
+//     console.log(totalUsd); // 输出 totalUsd 的值
+// } else {
+//     console.log('data or totalUsd property not found in the response');
+// }
 
-$notification.post('通知', '余额', totalUsd);
+// $notification.post('通知', '余额', totalUsd);
 $done({});
