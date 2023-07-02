e61c26fab31cf336dbc8aa3d48e68a0b636a4967
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-07-02 14:10:59 +0700

    1

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 $notification.post("通知","subtitle","你有一个通知");
-const requrl = $request;
+const requrl = JSON.parse($request);
 console.log(requrl);
 console.log(requrl.headers.User-Agent);
 $done({});
 const type = ['TronLink/1 CFNetwork/1408.0.4 Darwin/22.5.0']
-if (requrl.headers.User-Agent){
+if (requrl.headers['User-Agent']){
 
 }
 
