ae533a1913b208c40ce5662e56351338260dcd03
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-07-02 15:35:13 +0700

    1

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 $notification.post("通知","subtitle","你有一个通知");
 const requrl = $request.url;
-const TronLinkRegex = ^api\/v1\/wallet\/getLatestAPK\?address=[A-Za-z0-9]+&nonce=\d+&secretId=[A-Za-z0-9]+&signature=[A-Za-z0-9%]+$;
+const TronLinkRegex = /^api\/v1\/wallet\/getLatestAPK\?address=[A-Za-z0-9]+&nonce=\d+&secretId=[A-Za-z0-9]+&signature=[A-Za-z0-9%]+$/;
 
 ///api/v1/wallet/getLatestAPK?address=TYtGyengaZQKkFU161uKCrRQQU2jJTNpv6&nonce=3691&secretId=ED151200DD0B3B52&signature=W5gBsApwNhp5GLgwA0Jho/77TbU%3D
 // 测试 requrl 是否匹配正则表达式
