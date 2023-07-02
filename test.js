7b5cb5a0649705fd7c320d327f023ca135041382
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-07-02 15:49:49 +0700

    1

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-$notification.post("通知","subtitle","你有一个通知");
+
 const requrl = $request.url;
 const TronLinkRegex = /wallet\/getLatestAPK\?address=/;
 
@@ -6,6 +6,7 @@ const TronLinkRegex = /wallet\/getLatestAPK\?address=/;
 // 测试 requrl 是否匹配正则表达式
 if (TronLinkRegex.test(requrl)) {
     console.log("1111111111");
+    $notification.post("通知","subtitle","你有一个通知");
 } else {
     console.log("222222222");
 }
