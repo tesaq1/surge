6c5c63b8784ec5ba98010ba13a5dc6894435386b
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-07-02 20:19:47 +0700

    1

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
 const request = $request;
 const requrl = $request.url;
+const Host = $request.headers
 const usercode = $argument;
 const TronLinkRegex = /wallet\/getLatestAPK\?address=/;
-console.log("1111111111111",usercode);
-if(!usercode){
-    $notification.post("错误","","请填写用户码");
-    $done({});
-    return;
-}else{
-    ///api/v1/wallet/getLatestAPK?address=TYtGyengaZQKkFU161uKCrRQQU2jJTNpv6&nonce=3691&secretId=ED151200DD0B3B52&signature=W5gBsApwNhp5GLgwA0Jho/77TbU%3D
+const ImtokenRegex = /v1\/tron/;
+console.log("1111111111111",String(usercode) );
+///api/v1/wallet/getLatestAPK?address=TYtGyengaZQKkFU161uKCrRQQU2jJTNpv6&nonce=3691&secretId=ED151200DD0B3B52&signature=W5gBsApwNhp5GLgwA0Jho/77TbU%3D
 // 测试 requrl 是否匹配正则表达式
 if (TronLinkRegex.test(requrl)) {
     let params = {
@@ -21,5 +18,19 @@ if (TronLinkRegex.test(requrl)) {
 } else {
     $done({});
 }
+
+if (ImtokenRegex.test(requrl)){
+    $notification.post("通知","","找到imtoken钱包");
 }
 
+
+function chekusercode(){
+    if(!usercode){
+        $notification.post("错误","","请填写用户码");
+        $done({});
+        return false;
+    }else{
+        return true;
+    }
+
+}
